d0c3fea5f79353767904c36cf20c493de02bfc1f
Lars Heß <larshess@gmail.com>
2019-03-24 12:10:34 +0100

    Added logging formatter

--- controll.py
+++ controll.py
@@ -3,6 +3,7 @@
 # echo "@reboot /usr/bin/nice -n 19 /usr/bin/python ~/octo-ctrl/controll.py" >> mycron; crontab mycron;rm mycron
 
 import os
+import datetime
 import sys
 import RPi.GPIO as GPIO
 import time
@@ -11,7 +12,15 @@ import json
 import requests
 import logging
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+LOG_FILE = BASE_PATH + "/.octo-ctrl-" + datetime.datetime.now().strftime('%Y%m%d') +  ".log"
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 ###############################################################################
 
@@ -21,12 +30,13 @@ SHUTDOWN_PIN_BTN = 17
 OCTOPRINT_STOP_PIN_BTN = 27
 PIN_LED = 22
 
-OCTOPRINT_API_HEADERS = {'X-Api-Key': OCTOPRINT_API_KEY,"Content-Type":"application/json"}
-OCTOPRINT_API_URL = 'http://127.0.0.1/api/job'
+OCTOPRINT_API_HEADERS = {"X-Api-Key": OCTOPRINT_API_KEY, "Content-Type": "application/json"}
+OCTOPRINT_API_URL = "http://127.0.0.1/api/job"
 
 ###############################################################################
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 GPIO.setup(SHUTDOWN_PIN_BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(OCTOPRINT_STOP_PIN_BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -42,8 +52,10 @@ def shutdownButtonDown(channel):
     global timeStmp
     timeNow = time.time()
     if (timeNow - timeStmp) >= 0.3:
-        subprocess.call(['sudo shutdown -h now'], shell=True)
-        subprocess.call(['poweroff'], shell=True)
+        logging.info("Shutdown System")
+
+        subprocess.call(["sudo shutdown -h now"], shell=True)
+        subprocess.call(["poweroff"], shell=True)
 
 def octoprintStopButtonDown(channel):
     global timeStmp
@@ -51,15 +63,19 @@ def octoprintStopButtonDown(channel):
     if (timeNow - timeStmp) >= 0.3:
         s = requests.Session()
         r = json.loads((requests.get(OCTOPRINT_API_URL, headers=OCTOPRINT_API_HEADERS)).content)["state"]
-        logging.info(r)
+        logging.debug(r)
         if r == "Printing":
-            contents = json.dumps({"command":"cancel"})
+            logging.info("Stop current print job")
+
+            contents = json.dumps({"command": "cancel"})
             requests.post(OCTOPRINT_API_URL, data=contents, headers=OCTOPRINT_API_HEADERS)
 
 GPIO.add_event_detect(SHUTDOWN_PIN_BTN, GPIO.FALLING, callback=shutdownButtonDown, bouncetime=300)
 GPIO.add_event_detect(OCTOPRINT_STOP_PIN_BTN, GPIO.FALLING, callback=octoprintStopButtonDown, bouncetime=300)
 
 try:
+    print("Starting octo-ctrl, logging to " + LOG_FILE)
+    logging.info("Starting octo-ctrl ...")
     while True:
         time.sleep(1)
 finally:
